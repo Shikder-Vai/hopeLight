d18487145c18a53654e2ed5c80ac538068ee737f
unknown <Shikder-Vai>
2022-05-23 15:03:37 +0600

    revews section done

--- src/Pages/Home/Reviews/Review/Review.js
+++ src/Pages/Home/Reviews/Review/Review.js
@@ -1,23 +1,19 @@
 import React from "react";
-import people1 from "../../../../Assest/people1.png";
 
-const Review = () => {
+const Review = ({ review }) => {
+  const { reviews, img, user_name, country } = review;
   return (
     <div className="flex flex-col p-4 text-accent card bg-base-100 shadow-xl justify-center">
       <div>
-        <p>
-          It is a long established fact that by the readable content of a lot
-          layout. The point of using Lorem a more-or-less normal distribu to
-          using Content here, content
-        </p>
+        <p>{reviews}</p>
       </div>
       <div className="flex p-3 space-x-5 items-center">
         <div className=" outline outline-offset-2 outline-blue-500 rounded-full">
-          <img src={people1} alt="" />
+          <img src={img} alt="" />
         </div>
         <div>
-          <h2>Winson Herry</h2>
-          <p>California</p>
+          <h2>{user_name}</h2>
+          <p>{country}</p>
         </div>
       </div>
     </div>
--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -1,16 +1,19 @@
 import React from "react";
+import useReviews from "../../../hooks/useReviews";
 import Review from "./Review/Review";
 
 const Reviews = () => {
+  const [reviews] = useReviews();
+  console.log(reviews);
   return (
     <div className=" my-7">
       <div className=" my-7 flex justify-center items-center">
         <h1 className="text-2xl underline text-sky-300 ">Customer Reviews</h1>
       </div>
       <div className="grid p-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        <Review />
-        <Review />
-        <Review />
+        {reviews.slice(0, 6).map((review) => (
+          <Review review={review} />
+        ))}
       </div>
     </div>
   );
