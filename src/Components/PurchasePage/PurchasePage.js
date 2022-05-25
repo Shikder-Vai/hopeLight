62a3248ffb43cea4a8ed74846768b62d20d100a6
unknown <Shikder-Vai>
2022-05-26 01:31:09 +0600

    purches page updated

--- src/Components/PurchasePage/PurchasePage.js
+++ src/Components/PurchasePage/PurchasePage.js
@@ -1,22 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import bg from "../../Assest/footer.png";
 
 const PurchasePage = () => {
-  //   const [product, setProduct] = useState([]);
-  const productId = useParams();
+  const [product, setProduct] = useState([]);
+  const [refresh] = useState(0);
+  const { productId } = useParams();
   useEffect(() => {
-    fetch(`products.json/${productId}`)
+    fetch(`http://localhost:5000/Product/${productId}`)
       .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, [productId]);
+      .then((data) => setProduct(data));
+  }, [productId, refresh]);
+  const { name, img, description, quantity, lessOrder, price } = product;
   return (
     <div>
       <div>
         <div>
           <h1>Please Confirm Your Order</h1>
         </div>
-        <div>
-          <div className="card -z-30 bg-base-100 shadow-xl image-full">
+        <div
+          style={{ backgroundImage: `url(${bg})` }}
+          className=" my-7 flex justify-center items-center bg-no-repeat "
+        >
+          <div class="card glass card-side bg-base-100 shadow-xl">
+            <figure>
+              <img src={img} alt="" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">{name}</h2>
+              <h3 class="card-title">
+                <span className="text-xl font-bold">Price:</span>${price}
+              </h3>
+              <h3 class="card-title">
+                <span className="text-xl font-bold">Quantity:</span>${quantity}
+              </h3>
+              <h3 class="card-title">
+                <span className="text-xl font-bold">Minimum Order:</span>$
+                {lessOrder}
+              </h3>
+              <p className=" max-w-sm">{description}</p>
+            </div>
+          </div>
+          {/* <div className="card -z-30 bg-base-100 shadow-xl image-full">
             <figure>
               <img src={"img"} alt="" />
             </figure>
@@ -37,7 +62,7 @@ const PurchasePage = () => {
                 </h3>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
